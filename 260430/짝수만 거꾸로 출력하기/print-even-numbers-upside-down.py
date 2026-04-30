162a94e4aa8e519d44192fa39c582881f8a8fa48
rlaandzm6-collab <rlaandzm6@gmail.com>
2026-04-30 15:48:46 +0900

    Title: 짝수만 거꾸로 출력하기, Time: 0ms, Memory: 0MB, Status: Wrong Answer - Codetree

--- 260430/짝수만 거꾸로 출력하기/print-even-numbers-upside-down.py
+++ 260430/짝수만 거꾸로 출력하기/print-even-numbers-upside-down.py
@@ -1,10 +1,10 @@
 A=int(input())
 num=list(map(int,input().split()))
-num=num[::-1]
 
 
-for N in num:
-    if N % 2 == 0:
+
+for N in range(A-1,-1,-1):
+    if num[N] % 2 == 0:
         print(N,end=" ")
     
 
