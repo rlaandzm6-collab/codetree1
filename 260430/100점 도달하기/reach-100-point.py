c8403e891ad143ba5ba42c99f07d1d4e451703ce
rlaandzm6-collab <rlaandzm6@gmail.com>
2026-04-30 12:41:43 +0900

    Title: 100점 도달하기, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260430/100점 도달하기/reach-100-point.py
+++ 260430/100점 도달하기/reach-100-point.py
@@ -1,15 +1,13 @@
-# 변수 선언, 입력
 n = int(input())
 
-# 출력
-for i in range(n, 101):
-	if i >= 90:
-		print("A", end=" ")
-	elif i >= 80:
-		print("B", end=" ")
-	elif i >= 70:
-		print("C", end=" ")
-	elif i >= 60:
-		print("D", end=" ")
-	else:
-		print("F", end=" ")
+for i in range(n,101):
+    if i >= 90 :
+        print("A",end=" ")
+    elif i >=80 :
+        print("B",end=" ")
+    elif i >= 70 :
+        print("C",end=" ")
+    elif i >= 60 : 
+        print("D",end=" ")
+    else :
+        print("F",end=" ")
